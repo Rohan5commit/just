d4bd254623f451ac2254a677e14ae3f9c686b97e
Casey Rodarmor <casey@rodarmor.com>
2026-03-12 22:44:31 -0700

    Add more tests

--- tests/lazy.rs
+++ tests/lazy.rs
@@ -266,3 +266,226 @@ fn lazy_parameter_default_evaluated() {
     .stdout("default\n")
     .success();
 }
+
+#[test]
+fn lazy_multiple_invocations_evaluate_union_of_needed_variables() {
+  Test::new()
+    .justfile(
+      "
+        set lazy
+
+        x := 'foo'
+        y := 'bar'
+        unused := `exit 1`
+
+        foo:
+          @echo {{x}}
+
+        bar:
+          @echo {{y}}
+      ",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .args(["foo", "bar"])
+    .stdout("foo\nbar\n")
+    .success();
+}
+
+#[test]
+fn lazy_root_unused_not_evaluated_for_submodule_recipe() {
+  Test::new()
+    .justfile(
+      "
+        set lazy
+
+        unused := `exit 1`
+
+        mod foo
+      ",
+    )
+    .write(
+      "foo.just",
+      "bar:
+  @echo ok
+",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .args(["foo", "bar"])
+    .stdout("ok\n")
+    .success();
+}
+
+#[test]
+fn lazy_submodule_unused_not_evaluated_for_root_recipe() {
+  Test::new()
+    .justfile(
+      "
+        set lazy
+
+        foo:
+          @echo ok
+
+        mod bar
+      ",
+    )
+    .write(
+      "bar.just",
+      "unused := `exit 1`
+
+baz:
+  @echo baz
+",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .arg("foo")
+    .stdout("ok\n")
+    .success();
+}
+
+#[test]
+fn lazy_same_name_recipes_in_sibling_modules_both_evaluate() {
+  Test::new()
+    .justfile(
+      "
+        set lazy
+
+        mod foo
+        mod bar
+      ",
+    )
+    .write(
+      "foo.just",
+      "x := 'foo'
+unused := `exit 1`
+
+build:
+  @echo {{x}}
+",
+    )
+    .write(
+      "bar.just",
+      "y := 'bar'
+unused := `exit 1`
+
+build:
+  @echo {{y}}
+",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .args(["foo::build", "bar::build"])
+    .stdout("foo\nbar\n")
+    .success();
+}
+
+#[test]
+fn lazy_same_name_recipe_in_other_module_is_not_evaluated() {
+  Test::new()
+    .justfile(
+      "
+        set lazy
+
+        mod foo
+        mod bar
+      ",
+    )
+    .write(
+      "foo.just",
+      "x := 'foo'
+
+build:
+  @echo {{x}}
+",
+    )
+    .write(
+      "bar.just",
+      "unused := `exit 1`
+
+build:
+  @echo bar
+",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .arg("foo::build")
+    .stdout("foo\n")
+    .success();
+}
+
+#[test]
+fn lazy_evaluate_single_does_not_evaluate_unrelated_variable() {
+  Test::new()
+    .args(["--evaluate", "foo"])
+    .justfile(
+      "
+        set lazy
+
+        foo := 'bar'
+        unused := `exit 1`
+      ",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .stdout("bar")
+    .success();
+}
+
+#[test]
+fn lazy_evaluate_does_not_evaluate_private_unused_variable() {
+  Test::new()
+    .arg("--evaluate")
+    .justfile(
+      "
+        set lazy
+
+        foo := 'bar'
+
+        [private]
+        hidden := `exit 1`
+      ",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .stdout("foo := \"bar\"\n")
+    .success();
+}
+
+#[test]
+fn lazy_command_only_evaluates_exported_variables() {
+  Test::new()
+    .args(["--command", "sh", "-c", "printf $FOO"])
+    .justfile(
+      "
+        set lazy
+
+        export FOO := 'bar'
+        unused := `exit 1`
+
+        foo:
+          @echo foo
+      ",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .stdout("bar")
+    .success();
+}
+
+#[test]
+fn lazy_choose_only_evaluates_chosen_recipe_variables() {
+  Test::new()
+    .arg("--choose")
+    .env("JUST_CHOOSER", "head -n1")
+    .justfile(
+      "
+        set lazy
+
+        bar_value := 'bar'
+        foo_value := `exit 1`
+
+        foo:
+          @echo {{foo_value}}
+
+        bar:
+          @echo {{bar_value}}
+      ",
+    )
+    .env("JUST_UNSTABLE", "1")
+    .stdout("bar\n")
+    .success();
+}
